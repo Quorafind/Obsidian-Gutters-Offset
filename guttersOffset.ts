82ac6200af8448dac1bd1c877cc7328ad18869a6
Quorafind <bonopengate@gmail.com>
2023-07-10 14:17:06 +0800

    feat: support gutters offset

---
+++ guttersOffset.ts
@@ -0,0 +1,26 @@
+import { MarkdownView, Plugin, WorkspaceLeaf } from 'obsidian';
+
+export default class GuttersOffsetPlugin extends Plugin {
+	async onload() {
+		app.workspace.onLayoutReady(this.updateAllLeaves.bind(this));
+		app.workspace.on("layout-change", this.updateAllLeaves.bind(this));
+		app.workspace.on("active-leaf-change", (leaf)=> leaf && this.updateLeafOffset(leaf));
+	}
+
+	updateAllLeaves(){
+		app.workspace.getLeavesOfType("markdown").forEach((leaf)=> this.updateLeafOffset(leaf));
+	}
+
+	updateLeafOffset(leaf: WorkspaceLeaf) {
+		const view = leaf.view;
+		if(view instanceof MarkdownView){
+			const contentEl = view.contentEl;
+			const gutters = contentEl.querySelector(".cm-gutters") as HTMLElement;
+			contentEl.style.setProperty("--gutters-width", `${gutters.offsetWidth - 1}px`);
+		}
+	}
+
+	onunload() {
+
+	}
+}
